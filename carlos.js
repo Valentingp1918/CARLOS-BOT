204b4270f519b21fcc2d1a5d16cdbcc47808d198
Valentingp1918 <98440613+Valentingp1918@users.noreply.github.com>
2022-02-02 18:36:24 -0400

    Create carlos.js

---
+++ carlos.js
@@ -0,0 +1,8 @@
+const { WAConnection } =
+      require(´@adiwajshing/baileys`);
+      conts fs = require (`fs`)
+      asyn funtion iniciar ( ) {
+      const car = new WAConnection ( )
+      car.logger.level = "Advertir" 
+      //llamar al codigo QR
+      car. om ('qr', ( c) =>{
